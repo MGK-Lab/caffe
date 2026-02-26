3740c147bf76f4005e8888ad9eef954e65ea14b4
Maziar Korzani <m.gh.korzani@gmail.com>
2026-02-26 19:39:55 +1000

    conda updated

--- setup.py
+++ setup.py
@@ -1,32 +1,24 @@
 # This setup.py file is used to compile pyx files
-# python setup.py build_ext --inplace
+# python setup.py 
 
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-import Cython.Compiler.Options
-import numpy
 import subprocess
 
-
-Cython.Compiler.Options.annotate = True
-
-ext_modules = [
-    Extension("caffe_core", ["./src/caffe_core.pyx"])
-]
-
-
-setup(
-    ext_modules=cythonize(ext_modules,
-                          compiler_directives={'profile': True}),
-    include_dirs=[numpy.get_include()]
+# Compile the parallel C++ file
+process = subprocess.Popen(
+    [
+        'g++',
+        '-fopenmp',       # enable OpenMP
+        '-fPIC',          # generate position-independent code
+        './src/caffe_core_parallel.cpp',  # source file
+        '-shared',        # build shared library
+        '-o', 'caffe_core_parallel.so'    # output file
+    ],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE
 )
 
-# Run the g++ command to compile the C++ file
-process = subprocess.Popen(
-    ['g++', '-fopenmp', '-fPIC', "./src/caffe_core_parallel.cpp", '-shared',
-     '-o', 'caffe_core_parallel.so'],
-    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout, stderr = process.communicate()
+
 if process.returncode != 0:
     print("Compilation failed. Error message:")
     print(stderr.decode('utf-8'))
